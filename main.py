94c3cd78ab54b0545a8ab01d5b23be4a873e448f
Mrclive7406 <mrclive74062@gmail.com>
2025-06-05 15:28:33 +0300

    initialized cronjob and function for him

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from backend_app.schemas.item_model import Item
+import time
+import threading
 # from backend_app.core.config import settings
 # from backend_app.api.routers import main_router
 
@@ -32,9 +34,22 @@ async def create_item(item: Item):
     return {"id": item.id, "name": item.name, "amount": item.amount}
 
 
-# def cron_job():
-#     print("я крон")
+def cron_job():
+    while True:
+        print("я крон")
+        time.sleep(15)
 
+
+# Запуск крон-задачи в отдельном потоке при старте приложения
+def start_cron():
+    thread = threading.Thread(target=cron_job, daemon=True)
+    thread.start()
+
+
+# Вызовем при запуске приложения
+@app.on_event("startup")
+async def startup_event():
+    start_cron()
 
 # scheduler = BackgroundScheduler
 # scheduler.add_job(cron_job, "interval", seconds=60)
